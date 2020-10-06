e80160c050874b24f734de488edf043f4589f47e
Alan Parley <alanparley@hotmail.com>
2020-10-06 13:30:25 +0100

    save functionality completed for cities

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -21,14 +21,29 @@ def new_city():
 @cities_blueprint.route('/cities', methods=['POST'])
 def create_city():
     city_name = request.form['city_name']
-    visited = request.form['visited']
+    # visited = request.form['visited']
     country_id = request.form['country_id']
     country = country_repository.select(country_id)
-    city = City(city_name, country, visited)
+    city = City(city_name, country)
+    city_repository.save(city)
+    return redirect('/countries')
+
+@cities_blueprint.route("/cities/<id>/newtocountry", methods=['GET'])
+def new_city_to_country(id):
+    country = country_repository.select(id)
+    return render_template('cities/newtocountry.html', country = country)
+
+@cities_blueprint.route('/cities/new_to_country', methods=['POST'])
+def add_city_to_country():
+    city_name = request.form['city_name']
+    country_id = request.form['country']
+
+    country = country_repository.select(country_id)
+
+    city = City(city_name, country)
 
     city_repository.save(city)
-    return redirect('/cities')
-
+    return redirect('/countries')
 
 
 @cities_blueprint.route("/countries/<id>", methods=['GET'])
@@ -50,10 +65,10 @@ def update_city(id):
     visited = request.form['visited']
     city = City(city.city_name, country, visited, id)
     city_repository.update(city)
-    return redirect('/cities')
+    return redirect('/countries')
 
 @cities_blueprint.route("/cities/<id>/delete", methods=['POST'])
 def delete_city(id):
     city_repository.delete(id)
-    return redirect('/cities')
+    return redirect('/countries')
 
